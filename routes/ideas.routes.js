c4f79e925cf1c143a8995cc933eb3c1b0ce0b9a8
foxwayne1 <fox.wayne1@gmail.com>
2023-03-08 13:04:29 +0000

    added new idea to ideas array

--- routes/ideas.routes.js
+++ routes/ideas.routes.js
@@ -27,17 +27,13 @@ const ideas = [
 ]
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the RandomIdeas API' })
-})
-
-router.get('/api/ideas', (req, res) => {
   res.json({
     success: true,
     data: ideas,
   })
 })
 
-router.get('/api/ideas/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const idea = ideas.find(idea => idea.id === +req.params.id)
 
   if (!idea) {
@@ -48,6 +44,21 @@ router.get('/api/ideas/:id', (req, res) => {
     success: true,
     data: idea,
   })
+})
+
+// add an idea
+router.post('/', (req, res, next) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  }
+
+  ideas.push(idea)
+
+  res.json({ success: true, data: idea })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const ideasRoutes = require('./routes/ideas.routes')
 
 const app = express()
 
-app.use('/', ideasRoutes)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the RandomIdeas API' })
+})
+app.use('/api/ideas', ideasRoutes)
 
 app.listen(3000, () => {
   console.log(`listening at http://localhost:3000`)
