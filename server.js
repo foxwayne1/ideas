afa124f43d668d93d6de05941dc2606c195e813d
foxwayne1 <fox.wayne1@gmail.com>
2023-03-08 12:31:12 +0000

    added routes folder and exported router

--- server.js
+++ server.js
@@ -1,54 +1,9 @@
 const express = require('express')
+const ideasRoutes = require('./routes/ideas.routes')
 
 const app = express()
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive Newsletter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRodgers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM as and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2022-01-02',
-  },
-]
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the RandomIdeas API' })
-})
-
-app.get('/api/ideas', (req, res) => {
-  res.json({
-    success: true,
-    data: ideas,
-  })
-})
-
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find(idea => idea.id === +req.params.id)
-
-  if (!idea) {
-    return res.status(404).json({ success: false, error: 'Resource not found' })
-  }
-
-  res.json({
-    success: true,
-    data: idea,
-  })
-})
+app.use('/', ideasRoutes)
 
 app.listen(3000, () => {
   console.log(`listening at http://localhost:3000`)
